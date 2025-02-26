4149371aa6d834af7aff0bcf7a7615cc73ef0f4a
jamylak <jamylak@gmail.com>
2025-02-26 21:00:55 +1100

    print all dirs
    no error handling cause who cares

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <sys/dirent.h>
 
 int main() {
   const char *home = getenv("HOME");
   printf("%s", home);
+
+  DIR *dir = opendir(home);
+  struct dirent *entry;
+  while ((entry = readdir(dir)) != NULL) {
+    if (entry->d_type == DT_DIR) {
+      printf("%s\n", entry->d_name);
+    }
+    printf("%s\n", entry->d_name);
+  }
+
+  closedir(dir);
+  return 0;
 }
