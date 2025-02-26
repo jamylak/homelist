9565f006082c399cd65c2fb5ada136a669f0e652
jamylak <jamylak@gmail.com>
2025-02-26 23:26:22 +1100

    some queue stuff

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 // BFS Search over eg. Home Directory
 // for all nested git repos (with max nested limi)
+// #include <cstdint>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,10 +8,17 @@
 #include <dirent.h>
 #include <sys/dirent.h>
 
+#define ARENA_SIZE 1000000 * 128
+
 typedef struct {
-  uint16_t len;
-  char data[];
+    uint16_t len;
+    char data[];
 } __attribute__((aligned(64))) QueuePathEntry;
+
+typedef struct {
+    QueuePathEntry *entries;
+    size_t current;
+} Queue;
 
 void process_dir_into_queue(const char *dirpath) {
   struct dirent *entry;
@@ -22,14 +30,19 @@ void process_dir_into_queue(const char *dirpath) {
       printf("%s\n", entry->d_name);
 
       // Construct full path
-      char sub_path[1024];
-      snprintf(sub_path, sizeof(sub_path), "%s/%s", dirpath, entry->d_name);
+      uint16_t pathlen = strlen(dirpath) + 1 + entry->d_namlen;
+      // char sub_path[pathlen];
+      // snprintf(sub_path, sizeof(sub_path), "%s/%s", dirpath, entry->d_name);
     }
   }
   closedir(dir);
 }
 
 int main() {
+  // Just guessing average dir size as 128
+  QueuePathEntry *all_entries = malloc(ARENA_SIZE);
+  Queue queue = {.entries = all_entries, .current=0};
+  
   const char *home = getenv("HOME");
   printf("%s", home);
   process_dir_into_queue(home);
