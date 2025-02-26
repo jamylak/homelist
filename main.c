9c8112aa1f35f92e05f82d49934baff31b48344f
jamylak <jamylak@gmail.com>
2025-02-26 22:44:41 +1100

    restructure

--- main.c
+++ main.c
@@ -1,28 +1,37 @@
+// BFS Search over eg. Home Directory
+// for all nested git repos (with max nested limi)
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
 #include <sys/dirent.h>
 
-#define PATH_LIMIT = 1024
+typedef struct {
+  uint16_t len;
+  char data[];
+} __attribute__((aligned(64))) QueuePathEntry;
 
-int main() {
-  const char *home = getenv("HOME");
-  printf("%s", home);
-
-  DIR *dir = opendir(home);
-
+void process_dir_into_queue(const char *dirpath) {
   struct dirent *entry;
+
+  DIR *dir = opendir(dirpath);
+
   while ((entry = readdir(dir)) != NULL) {
     if (entry->d_type == DT_DIR && entry->d_name[0] != '.') {
       printf("%s\n", entry->d_name);
 
       // Construct full path
       char sub_path[1024];
-      snprintf(sub_path, sizeof(sub_path), "%s/%s", home, entry->d_name);
+      snprintf(sub_path, sizeof(sub_path), "%s/%s", dirpath, entry->d_name);
     }
   }
-
   closedir(dir);
+}
+
+int main() {
+  const char *home = getenv("HOME");
+  printf("%s", home);
+  process_dir_into_queue(home);
   return 0;
 }
