0049a84775482183a36f985b5cfb54d47b26a4d2
jamylak <jamylak@gmail.com>
2025-02-26 21:17:14 +1100

    cleanup

--- main.c
+++ main.c
@@ -28,16 +28,14 @@ int main() {
 
   struct dirent *entry;
   while ((entry = readdir(dir)) != NULL) {
-    if (entry->d_type == DT_DIR) {
+    if (entry->d_type == DT_DIR && entry->d_name[0] !=
+'.') {
       printf("%s\n", entry->d_name);
 
       // Construct full path
-
       char sub_path[1024];
       snprintf(sub_path, sizeof(sub_path), "%s/%s", home, entry->d_name);
       list_subdirs(sub_path);
-      
-      // list_subdirs(entry->d_name);
     }
     printf("%s\n", entry->d_name);
   }
